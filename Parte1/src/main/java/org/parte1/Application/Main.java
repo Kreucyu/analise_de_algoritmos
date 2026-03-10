5b370de5ca2ba58d90cb959f98d555acefb60477
Kreucyu <elciocleiton72@gmail.com>
2026-03-10 19:58:03 -0300

    Produto criado

--- Parte1/src/main/java/org/parte1/Application/Main.java
+++ Parte1/src/main/java/org/parte1/Application/Main.java
@@ -1,4 +1,4 @@
-package org.parte1;
+package org.parte1.Application;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
---
+++ Parte1/src/main/java/org/parte1/Model/Entities/Produto.java
@@ -0,0 +1,34 @@
+package org.parte1.Model.Entities;
+
+public class Produto {
+    private String _nomeProduto;
+    private Double _valorProduto;
+    private Double _pesoProduto;
+
+    public Produto(String nomeProduto, Double valorProduto, Double pesoProduto) {
+        this._nomeProduto = nomeProduto;
+        this._valorProduto = valorProduto;
+        this._pesoProduto = pesoProduto;
+    }
+
+    public String getNomeProduto() {
+        return _nomeProduto;
+    }
+
+    public Double getValorProduto() {
+        return _valorProduto;
+    }
+
+    public Double getPesoProduto() {
+        return _pesoProduto;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "_nomeProduto='" + _nomeProduto + '\'' +
+                ", _valorProduto=" + _valorProduto +
+                ", _pesoProduto=" + _pesoProduto +
+                '}';
+    }
+}
