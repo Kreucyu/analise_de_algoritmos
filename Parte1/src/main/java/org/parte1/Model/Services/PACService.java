752f1a24dba2aac9a1968a38b0eed133e4efc54a
Kreucyu <elciocleiton72@gmail.com>
2026-03-11 13:06:28 -0300

    Lógica de PAC e SEDEX implementadas

--- Parte1/src/main/java/org/parte1/Model/Services/PACService.java
+++ Parte1/src/main/java/org/parte1/Model/Services/PACService.java
@@ -3,6 +3,19 @@ package org.parte1.Model.Services;
 public class PACService implements TipoEntrega {
     @Override
     public Double calcularValorDoEnvio(Double PesoPedido) {
+        double valorEnvio = 0.0;
 
+        double umQuilo = 1.0;
+        if (PesoPedido <= umQuilo) {
+            valorEnvio = 10.0;
+            return valorEnvio;
+        }
+        double doisQuilos = 2.0;
+        if (PesoPedido >= umQuilo && PesoPedido <= doisQuilos) {
+            valorEnvio = 15.0;
+            return valorEnvio;
+        }
+        System.out.println("\nNão é possível fazer entregas acima de 2Kg!");
+        return valorEnvio;
     }
 }
--- Parte1/src/main/java/org/parte1/Model/Services/SEDEXService.java
+++ Parte1/src/main/java/org/parte1/Model/Services/SEDEXService.java
@@ -4,6 +4,25 @@ public class SEDEXService implements TipoEntrega {
 
     @Override
     public Double calcularValorDoEnvio(Double PesoPedido) {
+        double quinhentosGramas = 0.5;
+        double valorEnvio = 0.0;
+        if(PesoPedido <= quinhentosGramas){
+            valorEnvio = 12.50;
+            return valorEnvio;
+        }
 
+        double umQuilo = 1.0;
+        if(PesoPedido >= quinhentosGramas && PesoPedido <= umQuilo){
+            valorEnvio = 20.0;
+            return valorEnvio;
+        }
+        double adicional = 0.1;
+        valorEnvio = 46.50;
+        if(PesoPedido > umQuilo + adicional){
+            double valorAdicional = Math.round((PesoPedido - umQuilo) / adicional) * 1.5;
+            valorEnvio += valorAdicional;
+            return valorEnvio;
+        }
+        return valorEnvio;
     }
 }
