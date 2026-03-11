204ae6cb3e0efa1b414f45f93a4b0b39e7f6fbab
Kreucyu <elciocleiton72@gmail.com>
2026-03-11 12:06:17 -0300

    Método para calcular peso do pedido

--- Parte1/src/main/java/org/parte1/Model/Entities/Pedido.java
+++ Parte1/src/main/java/org/parte1/Model/Entities/Pedido.java
@@ -4,11 +4,28 @@ import java.util.ArrayList;
 
 public class Pedido {
     private Integer _idPedido;
-    private Integer _idCliente;
-    private ArrayList<Produto> _produtos;
+    private ArrayList<Produto> _listaDeProdutos;
 
-    public Pedido(Integer idPedido, Integer idCliente) {
+    public Pedido(Integer idPedido) {
         this._idPedido = idPedido;
-        this._idCliente = idCliente;
+        this._listaDeProdutos = new ArrayList<>();
+    }
+
+    public Integer get_idPedido() {
+        return _idPedido;
+    }
+
+    public void calcularValorDoPedido() {
+        Double pesoDoPedido = calcularPesoDoPedido();
+    }
+
+    public Double calcularPesoDoPedido() {
+        Double pesoDoPedido = 0.0;
+        for (Produto produto : _listaDeProdutos){
+            pesoDoPedido += produto.getPesoProduto();
+        }
+        return pesoDoPedido;
     }
 }
+
+
