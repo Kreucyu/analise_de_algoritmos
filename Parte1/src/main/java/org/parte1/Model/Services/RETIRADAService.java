32453ff9d8db1daf89843e0fe054795c50adf607
Kreucyu <elciocleiton72@gmail.com>
2026-03-11 15:30:58 -0300

    Refatoração do código, melhora na leitura e melhor coesão

---
+++ Parte1/src/main/java/org/parte1/Model/Services/RETIRADAService.java
@@ -0,0 +1,8 @@
+package org.parte1.Model.Services;
+
+public class RETIRADAService implements TipoEntrega{
+    @Override
+    public Double calcularValorDoEnvio(Double PesoPedido){
+        return 0.0;
+    }
+}
--- Parte1/src/main/java/org/parte1/Model/Services/SEDEXService.java
+++ Parte1/src/main/java/org/parte1/Model/Services/SEDEXService.java
@@ -1,25 +1,28 @@
 package org.parte1.Model.Services;
 
 public class SEDEXService implements TipoEntrega {
+    private static final double UM_QUILO = 1.0;
+    private static final double QUINTENTOS_GRAMA = 0.5;
+    private static final double PRECO_ATE_QUINHENTOS_GRAMA = 12.5;
+    private static final double PRECO_ATE_UM_QUILO = 20.0;
+    private static final double ADICIONAL = 0.1;
+    private static final double PRECO_ACIMA_DE_UM_QUILO = 46.50;
 
     @Override
     public Double calcularValorDoEnvio(Double PesoPedido) {
-        double quinhentosGramas = 0.5;
         double valorEnvio = 0.0;
-        if(PesoPedido <= quinhentosGramas){
-            valorEnvio = 12.50;
+        if(PesoPedido <= QUINTENTOS_GRAMA){
+            valorEnvio = PRECO_ATE_QUINHENTOS_GRAMA;
             return valorEnvio;
         }
 
-        double umQuilo = 1.0;
-        if(PesoPedido >= quinhentosGramas && PesoPedido <= umQuilo){
-            valorEnvio = 20.0;
+        if(PesoPedido >= QUINTENTOS_GRAMA && PesoPedido <= UM_QUILO){
+            valorEnvio = PRECO_ATE_UM_QUILO;
             return valorEnvio;
         }
-        double adicional = 0.1;
-        valorEnvio = 46.50;
-        if(PesoPedido > umQuilo + adicional){
-            double valorAdicional = Math.round((PesoPedido - umQuilo) / adicional) * 1.5;
+        valorEnvio = PRECO_ACIMA_DE_UM_QUILO;
+        if(PesoPedido > UM_QUILO + ADICIONAL){
+            double valorAdicional = Math.round((PesoPedido - UM_QUILO) / ADICIONAL) * 1.5;
             valorEnvio += valorAdicional;
             return valorEnvio;
         }
