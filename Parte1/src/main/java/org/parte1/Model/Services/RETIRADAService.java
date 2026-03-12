5b791bc203e2d87a6ff5ae98e5bda4e80b43e3f6
Elcio Wippel <elciocleiton72@gmail.com>
2026-03-12 16:22:05 -0300

    fix: limpeza no RETIRADAService para resolver alguns conflitos

--- Parte1/src/main/java/org/parte1/Model/Services/RETIRADAService.java
+++ Parte1/src/main/java/org/parte1/Model/Services/RETIRADAService.java
@@ -5,8 +5,4 @@ public class RETIRADAService implements TipoEntrega{
     public Double calcularValorDoEnvio(Double PesoPedido){
         return 0.0;
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 91b246bc07e34b8ae037b70b6fc68b298224faf9
 }
