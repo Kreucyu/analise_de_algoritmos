a391d347bf81d5016ec317d9294373afa71dfd39
Baphomet <fgrochovski@gmail.com>
2026-03-12 17:46:18 -0300

    feat: Ajustar PedidoTest para refletir mudanças feitas nas classes

--- Parte1/src/main/java/org/parte1/Application/Main.java
+++ Parte1/src/main/java/org/parte1/Application/Main.java
@@ -3,6 +3,7 @@ package org.parte1.Application;
 import org.parte1.Model.Entities.Pedido;
 import org.parte1.Model.Entities.Produto;
 import org.parte1.Model.Entities.TiposDeEntrega;
+import org.parte1.Model.Exceptions.PACIllegalWeightException;
 
 import java.util.ArrayList;
 
@@ -10,20 +11,44 @@ public class Main {
     public static void main(String[] args) {
         Produto produto1 = new Produto("O Senhor dos Anéis", 20.0, 0.3);
         Produto produto2 = new Produto("Holly", 30.0, 0.4);
-        Produto produto3 = new Produto("Alice no País das Maravilhas", 15.0, 0.2);
+        Produto produto3 = new Produto("Alice no País das Maravilhas", 15.0, 0.1);
         Produto produto4 = new Produto("It a Coisa", 50.0, 0.8);
 
-        ArrayList<Produto> produtos = new ArrayList<Produto>();
-        produtos.add(produto1);
-        produtos.add(produto2);
-        produtos.add(produto3);
-        produtos.add(produto4);
-        Pedido pedido1 = new Pedido(1, produtos, TiposDeEntrega.SEDEX);
+        Pedido pedido1 = new Pedido(1, TiposDeEntrega.SEDEX);
+        pedido1.adicionarProduto(produto1);
+        pedido1.adicionarProduto(produto2);
+        pedido1.adicionarProduto(produto3);
+        pedido1.adicionarProduto(produto4);
         System.out.println(pedido1.toString());
-        Pedido pedido2 = new Pedido(2, produtos, TiposDeEntrega.PAC);
-        System.out.println(pedido2.toString());
-        Pedido pedido3 = new Pedido(3, produtos, TiposDeEntrega.RETIRADA);
+
+        Pedido pedido2 = new Pedido(2,TiposDeEntrega.PAC);
+        pedido2.adicionarProduto(produto1);
+        pedido2.adicionarProduto(produto2);
+        pedido2.adicionarProduto(produto3);
+        pedido2.adicionarProduto(produto4);
+        try {
+            System.out.println(pedido2.toString());
+        } catch (PACIllegalWeightException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        Pedido pedido3 = new Pedido(3,TiposDeEntrega.RETIRADA);
+        pedido3.adicionarProduto(produto1);
+        pedido3.adicionarProduto(produto2);
+        pedido3.adicionarProduto(produto3);
+        pedido3.adicionarProduto(produto4);
         System.out.println(pedido3.toString());
+        
+        Produto produtoTeste = new Produto("Percy Jackson e o Ladrão de Raios", 34.5, 2.5);
+        Pedido pedido4 = new Pedido(4,TiposDeEntrega.PAC);
+        pedido4.adicionarProduto(produtoTeste);
+        try {
+            System.out.println(pedido4.toString());
+        } catch (PACIllegalWeightException e) {
+            System.out.println(e.getMessage());
+        }
+
 
 
     }
--- Parte1/src/test/java/org/parte1/Model/Entities/PedidoTest.java
+++ Parte1/src/test/java/org/parte1/Model/Entities/PedidoTest.java
@@ -1,11 +1,8 @@
 package org.parte1.Model.Entities;
 
 import org.junit.jupiter.api.Test;
+import org.parte1.Model.Exceptions.PACIllegalWeightException;
 import org.parte1.Model.Services.PACService;
-import org.parte1.Model.Services.SEDEXService;
-import org.parte1.Model.Services.RETIRADAService;
-
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,46 +10,76 @@ public class PedidoTest {
 
     @Test
     void calcularValorPedidoComSEDEX() {
-        Produto produto1 = new Produto("Produto 1", 10.0, 1.5);
-        Produto produto2 = new Produto("Produto 2", 20.0, 2.0);
-        ArrayList<Produto> produtos = new ArrayList<>();
-        produtos.add(produto1);
-        produtos.add(produto2);
+        Pedido pedido = new Pedido(1, TiposDeEntrega.SEDEX);
 
-        Pedido pedido = new Pedido(1, produtos, TiposDeEntrega.SEDEX);
+        pedido.adicionarProduto(new Produto("Produto 1", 10.0, 0.4));
+        pedido.adicionarProduto(new Produto("Produto 2", 20.0, 0.3));
 
-        double valorPedido = pedido.calcularValorDoPedido(produtos, TiposDeEntrega.SEDEX);
+        double valorPedido = pedido.calcularValorDoPedido();
 
-        assertTrue(valorPedido > 0, "O valor do pedido com SEDEX deve ser maior que 0");
+        assertTrue(valorPedido > 30.0, "O valor do pedido com SEDEX deve incluir o frete");
     }
 
     @Test
     void calcularValorPedidoComPAC() {
-        Produto produto1 = new Produto("Produto 1", 10.0, 1.5);
-        Produto produto2 = new Produto("Produto 2", 20.0, 2.0);
-        ArrayList<Produto> produtos = new ArrayList<>();
-        produtos.add(produto1);
-        produtos.add(produto2);
+        Pedido pedido = new Pedido(2, TiposDeEntrega.PAC);
 
-        Pedido pedido = new Pedido(2, produtos, TiposDeEntrega.PAC);
+        pedido.adicionarProduto(new Produto("Produto 1", 10.0, 0.5));
+        pedido.adicionarProduto(new Produto("Produto 2", 20.0, 0.5));
 
-        double valorPedido = pedido.calcularValorDoPedido(produtos, TiposDeEntrega.PAC);
+        double valorPedido = pedido.calcularValorDoPedido();
 
-        assertTrue(valorPedido > 0, "O valor do pedido com PAC deve ser maior que 0");
+        assertTrue(valorPedido > 30.0, "O valor do pedido com PAC deve incluir o frete");
     }
 
     @Test
     void calcularValorPedidoComRetirada() {
-        Produto produto1 = new Produto("Produto 1", 10.0, 1.5);
-        Produto produto2 = new Produto("Produto 2", 20.0, 2.0);
-        ArrayList<Produto> produtos = new ArrayList<>();
-        produtos.add(produto1);
-        produtos.add(produto2);
+        Pedido pedido = new Pedido(3, TiposDeEntrega.RETIRADA);
 
-        Pedido pedido = new Pedido(3, produtos, TiposDeEntrega.RETIRADA);
+        pedido.adicionarProduto(new Produto("Produto 1", 10.0, 1.5));
+        pedido.adicionarProduto(new Produto("Produto 2", 20.0, 2.0));
 
-        double valorPedido = pedido.calcularValorDoPedido(produtos, TiposDeEntrega.RETIRADA);
+        double valorPedido = pedido.calcularValorDoPedido();
 
-        assertEquals(30.0, valorPedido, "O valor do pedido com Retirada deve ser o valor dos produtos, sem custo de envio");
+        assertEquals(30.0, valorPedido, "O valor do pedido com retirada não deve ter frete");
+    }
+
+    @Test
+    void calcularPesoDoPedido() {
+        Pedido pedido = new Pedido(4, TiposDeEntrega.RETIRADA);
+
+        pedido.adicionarProduto(new Produto("Produto 1", 10.0, 1.5));
+        pedido.adicionarProduto(new Produto("Produto 2", 20.0, 2.0));
+
+        double pesoTotal = pedido.calcularPeso();
+
+        assertEquals(3.5, pesoTotal, "O peso total do pedido deve ser a soma dos pesos dos produtos");
+    }
+
+    @Test
+    void adicionarEremoverProduto() {
+        Pedido pedido = new Pedido(5, TiposDeEntrega.RETIRADA);
+
+        Produto p1 = new Produto("Produto 1", 10.0, 1.0);
+        Produto p2 = new Produto("Produto 2", 20.0, 1.5);
+
+        pedido.adicionarProduto(p1);
+        pedido.adicionarProduto(p2);
+
+        assertEquals(2.5, pedido.calcularPeso(), 0.001, "Peso deve ser 2.5Kg após adicionar os produtos");
+
+        pedido.removerProduto(p1);
+
+        assertEquals(1.5, pedido.calcularPeso(), 0.001, "Peso deve ser 1.5Kg após remover p1");
+        assertEquals(20.0, pedido.calcularValorDoPedido(), 0.001, "Valor deve ser 20.0 após remover p1");
+    }
+
+    @Test
+    void calcularValorPedidoPACAcimaDe2KgDeveLancarException() {
+        Pedido pedido = new Pedido(7, TiposDeEntrega.PAC);
+
+        pedido.adicionarProduto(new Produto("Produto Pesado", 50.0, 2.5));
+
+        assertThrows(PACIllegalWeightException.class, pedido::calcularValorDoPedido);
     }
 }
