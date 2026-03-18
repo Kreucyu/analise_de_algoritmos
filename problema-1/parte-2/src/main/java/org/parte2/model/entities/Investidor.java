905544114fd7e48d8a5d84ae7e913e059a866337
Kreucyu <elciocleiton72@gmail.com>
2026-03-18 14:10:47 -0300

    feat: SistemaDeAcoes e Investidor implementados

--- problema-1/parte-2/src/main/java/org/parte2/model/entities/Investidor.java
+++ problema-1/parte-2/src/main/java/org/parte2/model/entities/Investidor.java
@@ -1,4 +1,23 @@
 package org.parte2.model.entities;
 
 public class Investidor {
+    private String nomeInvestidor;
+
+    public Investidor(String nomeInvestidor) {
+        this.nomeInvestidor = nomeInvestidor;
+    }
+
+    public String getNomeInvestidor() {
+        return nomeInvestidor;
+    }
+
+    public void registrarOrdem() {
+        System.out.println("Qual o tipo da ordem? (compra/venda)");
+        Ordem ordem = new Ordem(this.nomeInvestidor, tipoOrdem, valorOrdem);
+    }
+
+    public void registrarEmUmaAcao() {
+        Acao acao = new Acao();
+
+    }
 }
--- problema-1/parte-2/src/main/java/org/parte2/model/services/SistemaDeAcoes.java
+++ problema-1/parte-2/src/main/java/org/parte2/model/services/SistemaDeAcoes.java
@@ -1,4 +1,22 @@
 package org.parte2.model.services;
 
+import org.parte2.model.entities.Acao;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SistemaDeAcoes {
+    private List<Acao> ListaDeAcoes;
+
+    public SistemaDeAcoes() {
+        this.ListaDeAcoes = new ArrayList<>();
+    }
+
+    public List<Acao> getListaDeAcao() {
+        return this.ListaDeAcoes;
+    }
+
+    public void adicionarAcao(Acao acao) {
+        this.ListaDeAcoes.add(acao);
+    }
 }
