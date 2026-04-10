2c7d09c19caeec2f180e26a6ff036ad4931a6900
Kreucyu <elciocleiton72@gmail.com>
2026-04-10 15:10:47 -0300

    feat: testes implementados da questão 5 caso 1

--- problema-2/parte1/src/main/java/org/parte1/Tests/PerformanceTest.java
+++ problema-2/parte1/src/main/java/org/parte1/Tests/PerformanceTest.java
@@ -3,6 +3,7 @@ package org.parte1.Tests;
 import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Random;
 
 public class PerformanceTest {
     public static void main(String[] args) {
@@ -12,6 +13,7 @@ public class PerformanceTest {
         questaoTresTesteUm();
         questaoTresTesteDois();
         //inserir aqui o teste 4
+        questaoCincoTesteUm();
         //inserir aqui o teste 6
     }
 
@@ -332,6 +334,90 @@ public class PerformanceTest {
         }
         double mediaValoresExecucoesEncadeada = somaValoresExecucoes / execucoes;
         System.out.println("3.1 Média Lista Encadeada para ser esvaziada (último elemento) em Milissegundos: " + String.format("%.3f", mediaValoresExecucoesEncadeada) + " ms");
+
+        double normal =  mediaValoresExecucoesNormal;
+        double encadeada = mediaValoresExecucoesEncadeada;
+
+        String sistemaMaisRapido = encadeada > normal ? "\nLista Normal" : "\nLista Encadeada";
+        double vezesMaisRapida = encadeada > normal ? encadeada/normal : normal / encadeada;
+        double porcentagemDeMelhora = (vezesMaisRapida - 1) * 100;
+
+        System.out.println(sistemaMaisRapido + " é mais rápida e é " + String.format("%.2f", vezesMaisRapida) + " vezes mais rápida ou " + String.format("%.2f", porcentagemDeMelhora) + "%");
+    }
+
+    //inserir aqui a questao 4
+
+    private static void questaoCincoTesteUm() {
+        System.out.println("\nQuestão 5-1:\n");
+        int elementos = 100_000;
+        int execucoes = 20;
+        int consultas = 10_000;
+        double[] valoresExecucoes = new double[execucoes];
+        long start = 0;
+        long end = 0;
+        double total = 0;
+        double somaValoresExecucoes = 0;
+
+
+        try {
+            System.gc();
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < execucoes; i++) {
+            ArrayList<Integer> listaNormal = new ArrayList<>();
+
+            for(int j = 0; j < elementos; j++) {
+                listaNormal.add(j);
+            }
+
+            start = System.nanoTime();
+            for(int j = 0; j < consultas; j++) {
+                int consulta = new Random().nextInt(0, +99_999);
+                listaNormal.get(consulta);
+            }
+            end = System.nanoTime();
+            total = (double) (end - start) / 1_000_000;
+            valoresExecucoes[i] = total;
+        }
+
+        for(int i = 0; i < execucoes; i++) {
+            somaValoresExecucoes += valoresExecucoes[i];
+        }
+        double mediaValoresExecucoesNormal = somaValoresExecucoes / execucoes;
+        System.out.println("5.1 Média Lista Normal para acessar os elementos em Milissegundos: " + String.format("%.3f", mediaValoresExecucoesNormal) + " ms");
+
+        somaValoresExecucoes = 0;
+
+        try {
+            System.gc();
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < execucoes; i++) {
+            LinkedList<Integer> listaEncadeada = new LinkedList<>();
+            for(int j = 0; j < elementos; j++) {
+                listaEncadeada.add(j);
+            }
+            start = System.nanoTime();
+            for(int j = 0; j < consultas; j++) {
+                int consulta = new Random().nextInt(0, +99_999);
+                listaEncadeada.get(consulta);
+            }
+            end = System.nanoTime();
+            total = (double) (end - start) / 1_000_000;
+            valoresExecucoes[i] = total;
+        }
+
+        for(int i = 0; i < execucoes; i++) {
+            somaValoresExecucoes += valoresExecucoes[i];
+        }
+        double mediaValoresExecucoesEncadeada = somaValoresExecucoes / execucoes;
+        System.out.println("5.1 Média Lista Encadeada para acessar os elementos em Milissegundos: " + String.format("%.3f", mediaValoresExecucoesEncadeada) + " ms");
 
         double normal =  mediaValoresExecucoesNormal;
         double encadeada = mediaValoresExecucoesEncadeada;
